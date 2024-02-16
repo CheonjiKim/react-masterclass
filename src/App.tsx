858653388230f02ff37cc1860c75b6c3c697bcc8
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2024-02-16 22:50:09 +0900

    5.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import React from "react";
-import { ReactDOM } from "react";
-import { styled } from "styled-components";
-import Circle from "./Circle";
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100px;
-  width: 100px;
-  justify-content: center;
-  align-items: center;
-  background-color: tomato;
-`;
+import Router from "./Router";
 
 function App() {
-  return (
-    <div>
-      <Circle bgColor="teal"></Circle>
-      <Circle bgColor="tomato"></Circle>
-      <Circle bgColor="teal"></Circle>
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
